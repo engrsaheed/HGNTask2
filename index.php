472e0bcff8cd5644468b0163405cb227b5a7437c
engrsaheed <engrsaheed@gmail.com>
2018-04-13 19:19:32 +0100

    For review purposes

---
+++ index.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+<head>
+	<title></title>
+    <link rel="stylesheet" type="text/css" href="css/default.css">
+    <link href="https://fonts.googleapis.com/css?family=Roboto|Roboto+Condensed|Roboto+Mono" rel="stylesheet"> 
+</head>
+<body>
+    <div  id="wrapper">
+        
+        <div  id="content"> 
+        
+	            <div id="nav">	
+
+
+	            	<h2>HGN INTERNSHIP 4</h2>
+	            	<p>HOME | PORTFOLIO</p>
+
+	            </div>
+
+	            <div id="oval"> </div>
+	            <h3> WEB | MOBILE</h3>
+	            <div id="square">
+	            	
+	            	<p>
+	            		"Every great developer you know got there by solving problems they were unqualified to solve until they actually did it." - Patrick McKenzie 
+	            	</p>
+
+ <?php
+ date_default_timezone_set('Africa/Algiers' );
+echo "The Time is " . date("h:i:sa")."<br>";
+echo "Todays Date is " . date("d.m.Y");
+?> 
+
+	            </div>
+        
+        </div>
+    
+    
+    </div>
+    
+</body>
+</html>
